08462dbd5b1ca54ece863e71de7be63c4e2e2bf0
anjumpathan <anjumpathan966@gmail.com>
2023-01-20 01:31:34 +0530

    q1

---
+++ variable.js
@@ -0,0 +1,7 @@
+//01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+//1.undefin
+var name;
+console.log(name); // undefined
+//2.string
+var name = "anjum";
+var subject = "js";
