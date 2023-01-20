e0ad3e2a0fbd1d06a98000a24d4b2e4e0c90fcde
anjumpathan <anjumpathan966@gmail.com>
2023-01-20 16:59:32 +0530

    comparison

---
+++ Q10/comparison.js
@@ -0,0 +1,23 @@
+//10. Figure out the result of the following comparison expression first without using console.log().After you decide the result confirm it using console.log()
+const a = 4 > 3;
+//true
+const b = 4 >= 3;
+//true
+const c = 4 < 3;
+//false
+const d = 4 <= 3;
+//false
+const e = 4 == 4;
+//true
+const f = 4 === 4;
+//true
+const g = 4 != 4;
+//false
+const h = 4 !== 4;
+//false
+const I = 4 != "4";
+//false
+const j = 4 == "4";
+//true
+const k = 4 === "4";
+false;
