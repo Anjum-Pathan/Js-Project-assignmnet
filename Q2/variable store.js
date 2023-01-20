af4c0d932b4238afbc54b9e3b2821932063630f9
anjumpathan <anjumpathan966@gmail.com>
2023-01-20 12:01:17 +0530

    q2 completed

---
+++ Q2/variable store.js
@@ -0,0 +1,9 @@
+//02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+const Firstname = "anjum";
+const Lastname = "pathan";
+let maritalstatus = "single";
+let country = "india";
+let age = "24";
+console.log(
+  `First Name is ${Firstname},\n Last Name is ${Lastname},\n Marital status is ${maritalstatus} ,country name is ${country} ,Age is ${24}`
+);
