5608b7868171f3540dd633548d4cbb04f127c13a
anjumpathan <anjumpathan966@gmail.com>
2023-01-20 13:35:06 +0530

    split()

---
+++ Q5/Split().js
@@ -0,0 +1,5 @@
+//05. Declare a varibale and assign string value to it and then split it into an array using split() method
+const namevar = "anjum";
+splited = namevar.split("");
+console.log(`String splitted into an Array: ${JSON.stringify(splited)}`);
+console.log(splited);
