698d1d07570571be06564f1899a40549fb772663
anjumpathan <anjumpathan966@gmail.com>
2023-01-20 15:56:34 +0530

    split,lastindexof

---
+++ Q5,Q6,Q7/Split().js
@@ -0,0 +1,17 @@
+//05. Declare a varibale and assign string value to it and then split it into an array using split() method
+const namevar = "anjum";
+splited = namevar.split("");
+console.log(`String splitted into an Array: ${JSON.stringify(splited)}`);
+console.log(splited);
+//06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+const companyname = "Facebook,Google,Microsoft,Apple,IBM,Oracle,Amazon";
+console.log(companyname.split(","));
+
+// 07 .Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+//First occurrence
+const skill = ["pw skills", "html", "css", "javascript"];
+console.log(skill.lastIndexOf("pw skills"));
+//last occurrence
+const skill1 = ["html", "css", "javascript", "pw skills"];
+console.log(skill1.lastIndexOf("pw skills"));
