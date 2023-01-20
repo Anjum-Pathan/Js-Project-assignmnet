6184d9b56d21af87109115d5a8676ae7242d3553
anjumpathan <anjumpathan966@gmail.com>
2023-01-20 10:59:01 +0530

    variable datatypes

--- variable.js
+++ variable.js
@@ -1,7 +1,23 @@
 //01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+//primitive data types
 //1.undefin
 var name;
 console.log(name); // undefined
 //2.string
-var name = "anjum";
+let nam = "anjum";
 var subject = "js";
+console.log(typeof (nam + subject));
+//3.boolean
+let a = 5;
+let b = 7;
+console.log(a != b);
+//4.null
+let char = String(null);
+console.log(char);
+//5.Number
+console.log(typeof (a + b));
+//non-primitive data types
+//1.object
+var obj1 = { a: 5, b: 6 };
+obj1[a] = 9;
+console.log(typeof obj1);
