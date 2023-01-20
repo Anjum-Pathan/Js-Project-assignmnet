158a8441589b1afa390813957562061d8d60f10b
anjumpathan <anjumpathan966@gmail.com>
2023-01-20 16:36:11 +0530

    truthy and falsy

---
+++ Q9/statment.js
@@ -0,0 +1,39 @@
+//09. Boolean value is either true or false.
+// - Write three JavaScript statement example which provide truthy value.
+//1.
+if ("true statment") {
+  console.log("return true statement");
+} else {
+  console.log("can't return true statement");
+}
+//2.
+if (7) {
+  console.log("return true statement");
+} else {
+  console.log("can't return true statement");
+}
+//3.
+if ([1]) {
+  console.log("return true statement");
+} else {
+  console.log("can't return true statement");
+}
+//- Write three JavaScript statement example which provide falsy value.
+//1.
+if (false) {
+  console.log("return true statement");
+} else {
+  console.log("return false statement");
+}
+//2.
+if (null) {
+  console.log("return true statement");
+} else {
+  console.log("return false statement");
+}
+//3.
+if ("") {
+  console.log("return true statement");
+} else {
+  console.log("return false statement");
+}
