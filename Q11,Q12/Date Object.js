ef1ef97f565bc375462653a6f1a53e1e7584ec8a
anjumpathan <anjumpathan966@gmail.com>
2023-01-20 20:30:28 +0530

    date and time object

--- Q11,Q12/Date Object.js
+++ Q11,Q12/Date Object.js
@@ -28,3 +28,23 @@ let past = new Date("1970-01-01");
 let now = new Date();
 let elapsed = now - past;
 console.log(elapsed / 1000);
+
+//12. Create a human readable time format using the Date time object
+//1- YYYY-MM-DD HH:mm
+const a = new Date();
+const s = a.toISOString();
+console.log(s);
+//2- DD-MM-YYYY HH:mm
+const b = a.toLocaleString("en-CA", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+});
+console.log(b);
+
+//3- DD/MM/YYYY HH:mm
+let c = a.toLocaleString();
+console.log(c);
